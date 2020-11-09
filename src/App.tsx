7253103f24094656213ae09cda64c2d7cd60eead
Hermil Anastacio <hermilanastacio@pinion.ph>
2020-11-09 11:20:42 +0800

    - Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useBoolean } from '@uifabric/react-hooks';
 import NotificationComponent from './NotificationComponent/NotificationComponent';
 
 function App() {
-  const [isCalloutVisible, { toggle: toggleIsCalloutVisible }] = useBoolean(false);
-
   return (
     <div>
       <div style={{backgroundColor:"#64a5ff", color:"#fff", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"20px 50px"}}>
         <h2 style={{margin:0}}>Notification App</h2>
-        <NotificationComponent isVisible={isCalloutVisible} onToggle={toggleIsCalloutVisible}/>
+        <NotificationComponent/>
       </div>
     </div>
   );
--- src/NotificationComponent/NotificationComponent.tsx
+++ src/NotificationComponent/NotificationComponent.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 import { RingerSolidIcon } from '@fluentui/react-icons';
 import { Callout, mergeStyleSets, DirectionalHint } from 'office-ui-fabric-react';
-
-interface INotificationComponent {
-  isVisible: boolean;
-  onToggle: () => void;
-}
+import { useBoolean } from '@uifabric/react-hooks';
 
 const styles = mergeStyleSets({
   buttonArea: {
@@ -21,22 +17,24 @@ const styles = mergeStyleSets({
   },
 });
 
-const NotificationComponent: React.FC<INotificationComponent> = ({ onToggle, isVisible }) => {
+const NotificationComponent: React.FC = () => {
+  const [isCalloutVisible, { toggle: toggleIsCalloutVisible }] = useBoolean(false);
+
   return(
     <div style={{position:"relative"}}>
-      <div style={{position:"relative", cursor:"pointer"}} onClick={onToggle}>
+      <div style={{position:"relative", cursor:"pointer"}} onClick={toggleIsCalloutVisible}>
         <div style={{backgroundColor:"red", position:"absolute", top:-5, right:-10, fontSize:12, fontWeight:"bold", width: 18, height:18, borderRadius:"50%", display:"flex", alignItems:"center", justifyContent:"center"}}>3</div>
         <RingerSolidIcon style={{fontSize:25}} className="bellBtn"/>
       </div>
       <React.Fragment>
-        {isVisible && (
+        {isCalloutVisible && (
           <Callout
             className={styles.callout}
             role="alertdialog"
             gapSpace={0}
             target={'.bellBtn'}
             directionalHint={DirectionalHint.bottomCenter}
-            onDismiss={onToggle}
+            onDismiss={toggleIsCalloutVisible}
             setInitialFocus
           >
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro itaque dolore nisi deserunt minus similique aliquam consequuntur minima nulla id error nam cum sunt delectus fugiat, accusantium eos magnam non?</p>
